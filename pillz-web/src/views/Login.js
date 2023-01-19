8b6ab6cd19be9bbf52b8bb89fafb60e205c69861
arjar88 <aryehakierman@gmail.com>
2023-01-19 12:35:42 +0200

    started working on login page

---
+++ pillz-web/src/views/Login.js
@@ -0,0 +1,12 @@
+import Grid from "@mui/material/Grid";
+import LoginCard from "../components/Login/LoginCard";
+
+function Login() {
+  return (
+    <Grid container justifyContent="center">
+      <LoginCard></LoginCard>
+    </Grid>
+  );
+}
+
+export default Login;
